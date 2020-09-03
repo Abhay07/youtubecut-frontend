765d3b0ea456df4c6945376e434a123e90b7f0e0
Abhay Srivastav <abhaysrivastav007@gmail.com>
2020-09-03 13:23:04 +0530

    fix

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://youtubecut.com/api/";
+export const API_URL = "https://api.youtubecut.com/";
 // export const API_URL = "http://localhost:8082/";
 export const TIME_FORMAT_ERROR = "Incorrect time, put time in seconds";
 export const END_TIME_ERROR = "End time should be greater than start time";
