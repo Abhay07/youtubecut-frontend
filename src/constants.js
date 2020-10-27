c0e56b3e25f19d48283263fbcff28f156460c5b2
Abhay Srivastav <abhaysrivastav007@gmail.com>
2020-10-27 12:50:21 +0530

    fix

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-// export const API_URL = "https://youtubecut.com/api/";
-export const API_URL = "http://localhost:8082/";
+export const API_URL = "https://youtubecut.com/api/";
+// export const API_URL = "http://localhost:8082/";
 export const TIME_FORMAT_ERROR = "Incorrect time, put time in seconds";
 export const END_TIME_ERROR = "End time should be greater than start time";
 export const INVALID_VIDEO_URL_ERROR = "Please enter a valid youtube video url";
